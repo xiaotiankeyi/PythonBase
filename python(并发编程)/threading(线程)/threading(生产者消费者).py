c4b02dd93b378926c4cb3e66cddf4f5a0dc925db
xiaotiankeyi <laizhitian163@163.com>
2021-07-17 17:08:19 +0800

    submit

---
+++ python(并发编程)/threading(线程)/threading(生产者消费者).py
@@ -0,0 +1,28 @@
+# 生产者消费者模式，保持相互之间的平衡
+
+from threading import Thread
+from queue import Queue
+from time import sleep
+
+def service():
+    num = 1
+    while True:
+        print('生产了{}杯奶茶'.format(num))
+        data.put(f'第{num}杯奶茶')
+        num += 1
+        sleep(1)
+
+def client():
+    print('购买了{}.....'.format(data.get()))
+    sleep(2)
+
+if __name__ == "__main__":
+    data = Queue(maxsize=10)
+
+    t1 = Thread(target=service, args="",)
+    t2 = Thread(target=client, args="",)
+    t3 = Thread(target=client, args="",)
+
+    t1.start()
+    t2.start()
+    t3.start()
