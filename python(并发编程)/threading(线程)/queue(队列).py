dddc830fa1950141f60ce10e5d8bf2b5623a9c9f
xiaotiankeyi <laizhitian163@163.com>
2021-07-17 16:28:21 +0800

    submit

--- python(并发编程)/threading(线程)/queue(队列).py
+++ python(并发编程)/threading(线程)/queue(队列).py
@@ -1,24 +1,55 @@
-"""
-概念：
-    针对多线性应用，队列是一种数据结构，
-"""
+# 概念： 针对多线性应用，队列是一种数据结构，
+
 import queue  # 线程队列
 
-q = queue.Queue(4)  # 三种储存取值模式，先进先出，先进后出，优先级
-q.put(12)
-q.put("hello")
-q.put({"name": "yuan"})
 
-#方法
-print(q.qsize())    #队列大小
-print(q.empty())    #是否为空
-print(q.full())     #是否满
+def one():
+    q = queue.Queue(4)  # 三种储存取值模式，先进先出
 
-while 1:
-    data = q.get()   #q.get(block=False)   取不到值就报错
-    print(data)
-    print("----------")
+    # 方法
+    # print(q.qsize())  # 队列大小
+    # print(q.empty())  # 是否为空
+    # print(q.full())  # 是否满
 
-import queue
+    # 存放数据
+    q.put(12)
+    q.put("hello")
+    q.put({"name": "yuan"})
 
-a = queue.LifoQueue     #先进后出模式
+    for i in range(4):
+        # q.get(block=False)   读取数据，队列为空时不报错
+        data = q.get(block=True, timeout=2)
+        # data = q.get_nowait()   #队列为空时报错
+        print(data)
+        print("----------")
+
+
+def two():
+    a = queue.LifoQueue()  # 先进后出模式
+    a.put(12)
+    a.put("hello")
+    a.put({"name": "yuan"})
+
+    for i in range(4):
+        data = a.get(block=True, timeout=2)
+        print(data)
+        print("----------")
+
+
+def three():
+    # 优先级，根据设定好的索引取值
+    f = queue.PriorityQueue()
+    f.put((1, 12))
+    f.put((3, "hello"))
+    f.put((2, {"name": "yuan"}))
+
+    for i in range(4):
+        data = f.get(block=True, timeout=2)
+        print(data)
+        print("----------")
+
+
+if __name__ == "__main__":
+    # one()
+    # two()
+    three()
