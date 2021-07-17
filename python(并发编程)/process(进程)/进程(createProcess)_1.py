5f0c8dc934b4449e3f1e42537c0d79a6bb3b4be3
xiaotiankeyi <laizhitian163@163.com>
2021-07-17 17:26:46 +0800

    submit

--- python(并发编程)/process(进程)/进程(createProcess)_1.py
+++ python(并发编程)/process(进程)/进程(createProcess)_1.py
@@ -1,23 +1,23 @@
 """通过队列来进行进程通讯"""
-import os, time, random
+import os
+import random
 # print(os.cpu_count())   #查看本机cpu
 
 from multiprocessing import Process  # multiprocessing"矛体帕赛醒"
 
 
-def func(name):
-    print("%s start operation func" % name)
+def func(name=None):
     print("主进程.......", os.getppid())
     print("子进程.......", os.getpid())
 
 
 if __name__ == "__main__":
     func("tom")
-
     print("=" * 20)
+
     p1 = Process(target=func, args=("jack",))
     p1.start()
-    # print(p1.name, p1.pid)
+    print(p1.name, p1.pid)
 
     # p2 = Process(target=func, args=("tom",))
     # p2.start()
--- python(并发编程)/process(进程)/进程(createProcess)_2.py
+++ python(并发编程)/process(进程)/进程(createProcess)_2.py
@@ -1,17 +1,19 @@
-import random,time
+import random
+import time
 
 from multiprocessing import Process
 
+
 class handler(Process):
-    def __init__(self,values):
+    def __init__(self, values):
         super(Process, self).__init__()
         self.values = values
 
     def run(self):
-        print(self.name,"%s start operation func" % self.values)
+        print(self.name, "%s start operation func" % self.values)
         # print("多进程测试.......",)
         print(time.sleep(random.randint(1, 4)))
-        print(self.name,"%s end operation func" % self.values)
+        print(self.name, "%s end operation func" % self.values)
 
 
 if __name__ == "__main__":
