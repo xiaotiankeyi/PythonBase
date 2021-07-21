c0259998faa9efc9c3360e80f1b7565b3e367204
xiaotiankeyi <laizhitian163@163.com>
2021-07-21 20:53:26 +0800

    submit

--- python(socket编程)/UDP/UDP_Client_one.py
+++ python(socket编程)/UDP/UDP_Client_one.py
@@ -2,13 +2,19 @@
 from socket import *
 
 ip_addr = ('127.0.0.1', 8080)
-accept_size = 1024
+accept_size = 1024      # 接收最大的字节数
 
 client = socket(AF_INET, SOCK_DGRAM)
+client.bind(("", 5050))     # 如果只是接收方一定要绑定ip
 
 while True:
     msg = input(">>>:").strip()
+
+    # sendto发送数据，发送数据需要转变为字节流encode
     client.sendto(msg.encode('utf8'), ip_addr)
 
     data, addr = client.recvfrom(accept_size)
     print(data.decode("utf8"))
+    print(addr)
+
+    # client.close()
--- python(socket编程)/UDP/UDP_Service.py
+++ python(socket编程)/UDP/UDP_Service.py
@@ -3,13 +3,16 @@ from socket import *
 ip_addr = ('127.0.0.1', 8080)
 accept_size = 1024
 
+# SOCK_DGRAM表示UDP协议
 server = socket(AF_INET, SOCK_DGRAM)
-server.bind(ip_addr)
+server.bind(ip_addr)    # 绑定IP地址
 
 # 接收客户端发来的"string"，然后转化为大写在发送回客户端
 
 while True:
+    # 接收客户端发送过的数据
     data, addr = server.recvfrom(accept_size)
     print(data.decode("utf8"))
+    print(addr)
 
     server.sendto(data.upper(), addr)
