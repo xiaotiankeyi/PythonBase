4c24f850e53b17a14485eac79c078a88f2a65937
xiaotiankeyi <laizhitian163@163.com>
2021-12-05 19:06:44 +0800

    WIN7提交

--- python_oo/oo_advanced/str_repr.py
+++ python_oo/oo_advanced/str_repr.py
@@ -1,18 +1,22 @@
 # 自定义__doc__,返回对象的描述信息
 
 class Foo(object):
-    pass
 
     def __str__(self):  # 可以自定义修改return
+        # objList = [obj, obj1]打印的是地质
         return '可以自定义修改return对象描述信息'
 
     def __repr__(self):
-        return '在打印类的时候，控制类的输入......'
+        # objList = [obj, obj1]打印的是return值
+        return f'在打印类的时候，控制类的输出......'
 
 
 if __name__ == "__main__":
     obj = Foo()
+    obj1 = Foo()
+    objList = [obj, obj1]
 
+    print(objList)
     print(dir(Foo))
     print(Foo.__dict__)
     print('获取对象描述信息:', obj)
