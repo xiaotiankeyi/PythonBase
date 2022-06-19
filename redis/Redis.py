e495eb658fab5517cb1adf56ff6c54ee56ec6f5b
xiaotiankeyi <laizhitian163@163.com>
2022-06-19 13:21:39 +0800

    6/19windows提交

--- redis/Redis.py
+++ redis/Redis.py
@@ -5,9 +5,9 @@ from redis import StrictRedis
 # conn = Redis(host='192.168.0.121', port=6380, db=0)
 conn = Redis(host='192.168.0.121', port=6379, db=0, decode_responses=True)
 
-# print(conn)
-# print(conn.keys())
-print(conn.get('k3'))
+print(conn)
+print(conn.keys())
+# print(conn.get('k3'))
 # conn.set('name','tom')
 
 if __name__ == "__main__":
